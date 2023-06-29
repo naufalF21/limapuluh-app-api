1b62b0940de255dc6680c53dd72939a5ce3a738b
naufalF21 <naufalfirman098@gmail.com>
2023-06-30 01:12:11 +0700

    first commit

---
+++ pages/api/questions.js
@@ -0,0 +1,56 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+	res.status(200).json([
+		{
+			id: 1,
+			name: 'Apakah anda merasa nyaman bekerja dengan alat-alat, seperti peralatan taman, peralatan pertanian, atau peralatan konstruksi?',
+			isOutdoor: true,
+		},
+		{
+			id: 2,
+			name: 'Apakah anda tertarik dengan pekerjaan yang melibatkan penggunaan teknologi dan perangkat lunak kantor?',
+			isOutdoor: false,
+		},
+		{
+			id: 3,
+			name: 'Apakah anda menikmati berinteraksi dengan alam dan lingkungan luar?',
+			isOutdoor: true,
+		},
+		{
+			id: 4,
+			name: 'Apakah anda merasa nyaman dan fokus saat bekerja dalam lingkungan yang lebih terkendali?',
+			isOutdoor: false,
+		},
+		{
+			id: 5,
+			name: 'Apakah anda tertarik dengan pekerjaan yang melibatkan eksplorasi alam?',
+			isOutdoor: true,
+		},
+		{
+			id: 6,
+			name: 'Apakah anda lebih suka bekerja di kantor atau lingkungan perkantoran yang terorganisir?',
+			isOutdoor: false,
+		},
+		{
+			id: 7,
+			name: 'Apakah anda siap menghadapi tantangan dan risiko yang terkait dengan pekerjaan anda, seperti paparan lingkungan berbahaya atau bahaya alam?',
+			isOutdoor: true,
+		},
+		{
+			id: 8,
+			name: 'Apakah anda memiliki kemampuan dan ketertarikan dalam menggunakan perangkat lunak dan alat bantu seperti spreadsheet, presentasi, atau perangkat lunak desain grafis?',
+			isOutdoor: false,
+		},
+		{
+			id: 9,
+			name: 'Apakah anda bisa bekerja dengan menyesuaikan kondisi cuaca yg sedang terjadi?',
+			isOutdoor: true,
+		},
+		{
+			id: 10,
+			name: 'Apakah saya lebih suka berinteraksi dengan orang-orang melalui komunikasi tertulis, email, atau video conference daripada secara langsung?',
+			isOutdoor: false,
+		},
+	]);
+}
