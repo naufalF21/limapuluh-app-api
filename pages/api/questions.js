998c3380c898ec89f35ffeac629324f2196196dc
naufalF21 <naufalfirman098@gmail.com>
2023-06-30 19:07:09 +0700

    update new api

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -3,16 +3,18 @@ import { question } from '@/question';
 import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
-	// await NextCors(req, res, {
-	// 	// options
-	// 	methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-	// 	origin: '*',
-	// 	optionsSuccessStatus: 200,
-	// });
-
-	// res.status(200).json(question);
-	return NextCors(req, res.json(question), {
+	await NextCors(req, res, {
+		// options
+		// methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+		// origin: '*',
+		// optionsSuccessStatus: 200,
 		status: 200,
 		headers: { 'Content-Type': 'application/json' },
 	});
+
+	return res.status(200).json(question);
+	// return NextCors(req, res.json(question), {
+	// 	status: 200,
+	// 	headers: { 'Content-Type': 'application/json' },
+	// });
 }
